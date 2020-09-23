6ceffa9f8753ef43657785486f4609c65c819cea
Timothy Axford <35697045+TAxford@users.noreply.github.com>
2020-09-23 15:25:40 -0400

    Added button and MouseEnter code
    "the mouse over me"

--- Buttons/Buttons/Form1.vb
+++ Buttons/Buttons/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+    End Sub
+
+    Private Sub Button1_MouseEnter(sender As Object, e As EventArgs) Handles Button1.MouseEnter
+        Button1.Text = "the mouse of over me"
+    End Sub
 End Class
