ee6f336e9336493a91789db1db233d3af5719ca4
Timothy Axford <35697045+TAxford@users.noreply.github.com>
2020-09-23 15:27:26 -0400

    Added Mouse Leave command
    "The mouse left"

--- Buttons/Buttons/Form1.vb
+++ Buttons/Buttons/Form1.vb
@@ -6,4 +6,8 @@
     Private Sub Button1_MouseEnter(sender As Object, e As EventArgs) Handles Button1.MouseEnter
         Button1.Text = "the mouse of over me"
     End Sub
+
+    Private Sub Button1_MouseLeave(sender As Object, e As EventArgs) Handles Button1.MouseLeave
+        Button1.Text = "The mouse left"
+    End Sub
 End Class
